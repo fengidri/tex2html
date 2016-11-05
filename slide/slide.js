6b21fab4fe98038718df304027a53fe3b594b214
fengidri <fengidri@gmail.com>
2016-11-05 13:48:54 +0000

    fix output pdf

--- slide/slide.js
+++ slide/slide.js
@@ -5,7 +5,6 @@
 //
 
 var SLIDES = []; // 用于保存所有的 H3 对象
-var Position;
 var HEIGHT;// = document.body.clientHeight;
 
 // 判断各种浏览器，找到正确的方法
@@ -29,77 +28,110 @@ function FullEvent(fun)
     document.addEventListener("msfullscreenchange",     fun, false);
 }
 
-function AddTools()
+function slice_bottom(n)
 {
     var pos = $('<div>')
     pos.css('margin', 0);
     pos.css('padding', 0);
     pos.css('border', 'None');
     pos.css('display', 'inline-block');
-    pos.css('position', 'fixed');
+    pos.css('position', 'absolute');
     pos.css('bottom', 12);
     pos.css('right', 12);
-    Position = pos;
+    pos.css('cursor', 'pointer');
     $('body').append(pos);
 
-    var full = $('<div>').text("Full");
-    full.css('margin', 0);
-    full.css('padding', 0);
-    full.css('border', '1px');
-    full.css('display', 'inline-block');
-    full.css('position', 'fixed');
-    full.css('bottom', 42);
-    full.css('right', 12);
-    full.css('cursor', 'pointer');
-    $('body').append(full);
+    //var full = $('<div id=Full>').text("Full");
+    //full.css('margin', 0);
+    //full.css('padding', 0);
+    //full.css('border', '1px');
+    //full.css('display', 'inline-block');
+    //full.css('position', 'fixed');
+    //full.css('bottom', 42);
+    //full.css('right', 12);
+    //full.css('cursor', 'pointer');
+    //$('body').append(full);
 
-    full.click(function(){
+    pos.click(function(){
         // 启动全屏!
         launchFullScreen(document.documentElement); // 整个网页
-        full.hide();
+    //    full.hide();
         //launchFullScreen(document.getElementById("videoElement")); // 某个页面元素
     });
+    pos.text(n)
+
+    return pos;
 
 }
 
 function page(p)
 {
     var offset = $(document).scrollTop()
-    var n = offset / HEIGHT;
+    var n = offset / document.body.clientHeight;
     n = Math.floor(n)
-    gotoslide(n + 1);
+    n = n + p;
+    gotoslide(n);
+    localStorage.setItem("current_page_num", n );
 }
 
 function gotoslide(n)
 {
     console.log("Goto Slide:", n, t);
+    n = n * 1;
 
     var t = SLIDES[n];
     if (!t) return;
 
     $(document).scrollTop($(t).offset().top);
 
-    Position.text(n + 1 + "/" + SLIDES.length);
 }
 
-function mk_one_slide(text)
+function mkbox(vector, height)
 {
-    var vector = $('body');
-    var slide_cur = $('<div class=slide>');
+    //-----------------
+    // header
+    //-----------------
+    // content
+    //
+    //-----------------
+    // bottom
+    //-----------------
+    var box = $('<div>');
+    vector.append(box);
 
-    slide_cur.css('height', HEIGHT);
-    slide_cur.css('width', HEIGHT);
+    if (height)
+    {
+        box.css('height', height);
+        box.css('width', height);
+    }
 
-    SLIDES.push(slide_cur[0]);
+    box.css('position', "relative");
+    box.css('padding', "0");
+    box.css('margin-top', "0");
+    box.css('margin-bottom', "0");
 
-    vector.append(slide_cur);
+    var header = $('<div>');
+    var content = $('<div>');
+    var bottom = $('<div>');
 
-    var slide_next = $('<div>');
-    slide_next.css('width', HEIGHT);
+    content.css('width', '100%');
 
-    slide_cur.append($('<H3>').text(text));
-    slide_cur.append(slide_next);
-    return slide_next;
+    bottom.css('margin', 0);
+    bottom.css('padding', 0);
+    bottom.css('border', 'None');
+    //bottom.css('display', 'inline-block');
+    bottom.css('position', 'absolute');
+    bottom.css('bottom', 10);
+    bottom.css('text-align', "right");
+    bottom.css('width', '100%');
+    //bottom.css('right', 12);
+    //bottom.css('cursor', 'pointer');
+
+    box.append(header);
+    box.append(content);
+    box.append(bottom);
+
+    return [box, header, content, bottom];
 }
 
 function Slides(objs)
@@ -110,8 +142,8 @@ function Slides(objs)
         HEIGHT = document.body.clientHeight;
     console.log("HEIGHT: ", HEIGHT);
 
-    var slide_cur = undefined;
-    var slide_item_nu = 0;
+    var slice_number = 0;
+    var content, box;
 
     objs.each(function()
         {
@@ -124,71 +156,44 @@ function Slides(objs)
             }
             if ('H3' == nodeName)
             {
-                if (slide_item_nu > 0) slide_item_nu = 0;
-                slide_cur = mk_one_slide(chapter_name + " - " + node.text());
+                slice_number += 1;
+                box = mkbox($('body'), document.body.clientHeight);
+
+                if (chapter_name.length > 0)
+                {
+                    node.text(chapter_name + '-' + node.text())
+                }
+
+                box[1].append(node);
+                box[3].append(slice_number);
+                content = box[2];
+                box[0].attr('class', 'slide')
+                content.attr('class', "content")
+
+                SLIDES.push(box[0][0]);
                 return;
             }
-            slide_item_nu += 1;
-            if (slide_cur) slide_cur.append(node);
+            if ('H4' == nodeName)
+            {
+                content[0]._no_resize = 1;
+            }
+
+            if (content) content.append(node);
         }
     );
 
-    scale($("div.slide"));
+    adjust_content();
+
+    //scale($("div.slide_content"));
     prettyPrint();
 }
 
-function makeslide(slides, heights)
-{
-    var heade_pre = 0;
-    for (var i in slides)
-    {
-        var node = slides[i];
-        var height = heights[i];
-        var space = HEIGHT - height;
 
-        console.log("height:", height, " space:", space, " makeslide :", node.text());
-
-        node.before(make_space(heade_pre));
-        node.after(make_space(space * 0.3));
-        heade_pre = space * 0.7;
-    }
-    $('body').append(make_space(heade_pre));
-}
-
-function make_space(h)
-{
-    var space = $('<div id=_space >')
-    space.css('margin', 0);
-    space.css('padding', 0);
-    space.css('border', 'None');
-    space.css('height', h);
-    return space;
-}
 
 function slide_resize(obj)
 {
-    var sub = obj.find('div');
+    //var obj = obj.find('div');
 
-    sub.css("transform", "None");
-    sub.css("transform-origin", "0% 0%");
-
-    if (sub.width() > HEIGHT)
-    {
-        var hsub = Math.max(sub.height(), sub.width());
-    }
-    else{
-        var hsub = sub.height();
-    }
-
-    var hmax = obj.height() - sub.offset().top + obj.offset().top - 20;
-
-    if (hsub > hmax)
-    {
-        sub.css("transform", "scale(" +  hmax / hsub + ")");
-    }
-    else if (hsub < hmax * 2 /3 ){
-        sub.css("transform", "translateY(" +  (hmax - hsub)* 2/5 + "px)");
-    }
 }
 
 function slide_all_resize()
@@ -212,25 +217,40 @@ function slide_img_resize()
     slide_resize($(this).parents(".slide"));
 }
 
-function scale(objs)
+function adjust_content()
 {
-    objs.each(function(){
-        slide_resize($(this));
-    });
+    var H = document.body.clientHeight;
+    $(".content").each(function(){
+        if (this._no_resize) return;
+        var obj = $(this);
 
+        obj.css("transform", "None");
+        obj.css("transform-origin", "0% 0%");
+
+        if (obj.height() > H * 0.8)
+        {
+            return;
+        }
+
+        obj.css("transform", "translateY(" +  (H - obj.height())* 2/5 + "px)");
+    });
 }
 
 function slide_show()
 {
     Slides($("#slide_orgin >  *"));
-    $("div.slide img").load(slide_img_resize);
-    AddTools();
+    $("img").load(adjust_content);
+    //AddTools();
 
-    gotoslide(0);
+    var n = localStorage.getItem("current_page_num")
+    if (!n)
+        n = 0
+    gotoslide(n);
 
     $('body').keypress(function(event){
         event.preventDefault();
-        if (32 == event.keyCode)
+        console.log(event.keyCode)
+        if (32 == event.keyCode) // space
         {
             page(1);
             //event.returnValue = false;
@@ -242,7 +262,7 @@ function slide_show()
             page(-1);
         }
     });
-    $(window).resize(slide_all_resize);
+//    $(window).resize(slide_all_resize);
 
     //FullEvent(function () {
     //    if (HEIGHT == document.body.clientHeight)
@@ -255,6 +275,8 @@ function slide_show()
     //    }
     //    slide_all_resize();
     //});
-
+    //
 }
+
+
 
